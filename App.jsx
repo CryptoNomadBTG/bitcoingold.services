7c6743da0b80a625737cb5aca44893a9d96facee
Ubuntu <ubuntu@ip-172-31-16-36.eu-north-1.compute.internal>
2025-12-28 11:05:48 +0000

    Initial commit from EC2

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import { BuilderComponent } from '@builder.io/react'
+import './App.css'
+
+function App() {
+  return (
+    <BuilderComponent model="page" />
+  )
+}
+
+export default App
---
+++ src/builder.js
@@ -0,0 +1,3 @@
+import { builder } from '@builder.io/react';
+
+builder.init('88f22d4a63384a518eca58d52971254b');
